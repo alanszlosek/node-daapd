fb977d470113b2f67755de297b5b8c1834b4e01c
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2015-02-17 21:14:11 -0500

    iTunes 12 wants: mDNS txtRecord, non-zero session id, revision > 1, update connection that stays open. other cleanups.

--- eg/server.js
+++ eg/server.js
@@ -3,8 +3,9 @@ var Song = require('../lib/song').Song;
 
 daap.createServer({
   advertise:true,
-  songs: [new Song({
-          file: 'music/short.mp3'
-        })],
-  name:"spruce"
+  songs: [
+    new Song({
+      file: 'music/short.mp3'
+    }),
+  ]
 }).listen(3689);
