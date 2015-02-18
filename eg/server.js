f2edf66fdb5273344d067751460a881148515daa
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2015-02-17 21:28:24 -0500

    TODO, readme, debugging cleanups

--- eg/server.js
+++ eg/server.js
@@ -7,5 +7,8 @@ daap.createServer({
     new Song({
       file: 'music/short.mp3'
     }),
+    new Song({
+      file: '/home/alan/Music3/Dillinger_Escape_Plan-When_Good_Dogs_Do_Bad_Things.mp3'
+    }),
   ]
 }).listen(3689);
