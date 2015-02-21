462bcfc33d55257c9f869a14374b447a8dee59e2
Alan Szlosek <alan.szlosek@gmail.com>
2015-02-21 10:21:40 -0500

    musicmetadata module for id3 tags, walk music folder in search of songs

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ node-daapd
 ==========
 
 A DAAP Server written for Node.js
+
+Required modules: mdns, walk, musicmetadata (for reading metadata from mp3, ogg, flac files)
--- eg/server.js
+++ eg/server.js
@@ -1,11 +1,62 @@
-var daap = require('../lib/daap');
-var Song = require('../lib/song').Song;
+var walk = require('walk'),
+  fs = require('fs'),
+  options,
+  songs = [],
+  walker,
+  arguments = process.argv.splice(2),
+  directory = arguments[0],
+  daap = require('../lib/daap'),
+  Song = require('../lib/song').Song;
 
-daap.createServer({
-  advertise:true,
-  songs: [
-    new Song({
-      file: 'music/short.mp3'
-    }),
-  ]
-}).listen(3689);
+options = {
+  followLinks: false,
+};
+if (!directory) {
+  console.log('usage: node server.js FOLDER');
+  return;
+}
+
+console.log('Sourcing from ' + directory);
+walker = walk.walk(directory, options);
+
+var counter = 0;
+walker.on("file", function (root, fileStats, next) {
+  var format,
+    song;
+  // do we need to check that fileStats.type == 'file'?
+
+  // For now, we'll support jpg covert art, mp3 and flac files
+  if (fileStats.name.match(/\.mp3$/i)) {
+    format = 'mp3';
+  } else if (fileStats.name.match(/\.flac$/i)) {
+    format = 'flac';
+  } else if (fileStats.name.match(/cover\.jpg$/i)) {
+    // covert art
+    format = 'jpg';
+  }
+  if (format) {
+    song = new Song(
+      root + '/' + fileStats.name,
+      fileStats.name,
+      fileStats.size,
+      format
+    );
+    songs.push(song);
+    song.read_tags(next);
+  
+  } else {
+    next();
+  }
+});
+
+walker.on("errors", function (root, nodeStatsArray, next) {
+  next();
+});
+
+walker.on("end", function () {
+  daap.createServer({
+    advertise: true,
+    songs:songs 
+  }).listen(3689);
+});
+
